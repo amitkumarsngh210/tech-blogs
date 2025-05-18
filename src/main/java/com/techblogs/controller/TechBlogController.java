e3013d4ce00f04fa28e8136c02fe813b2aff1967
Amit Kumar <amit.kumar@finalsite.com>
2025-05-18 11:24:38 +0530

    add logging framework

--- src/main/java/com/techblogs/controller/TechBlogController.java
+++ src/main/java/com/techblogs/controller/TechBlogController.java
@@ -4,6 +4,8 @@ import com.techblogs.model.TechBlog;
 import com.techblogs.service.TechBlogService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,41 +17,62 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TechBlogController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TechBlogController.class);
     private final TechBlogService techBlogService;
 
     @PostMapping
     public ResponseEntity<TechBlog> createBlog(@Valid @RequestBody TechBlog techBlog) {
-        return new ResponseEntity<>(techBlogService.createBlog(techBlog), HttpStatus.CREATED);
+        logger.debug("Received request to create new blog post with title: {}", techBlog.getTitle());
+        TechBlog createdBlog = techBlogService.createBlog(techBlog);
+        logger.info("Successfully created blog post with id: {}", createdBlog.getId());
+        return new ResponseEntity<>(createdBlog, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<TechBlog> updateBlog(@PathVariable Long id, @Valid @RequestBody TechBlog techBlog) {
-        return ResponseEntity.ok(techBlogService.updateBlog(id, techBlog));
+        logger.debug("Received request to update blog post with id: {}", id);
+        TechBlog updatedBlog = techBlogService.updateBlog(id, techBlog);
+        logger.info("Successfully updated blog post with id: {}", id);
+        return ResponseEntity.ok(updatedBlog);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TechBlog> getBlogById(@PathVariable Long id) {
-        return ResponseEntity.ok(techBlogService.getBlogById(id));
+        logger.debug("Received request to fetch blog post with id: {}", id);
+        TechBlog blog = techBlogService.getBlogById(id);
+        logger.info("Successfully retrieved blog post with id: {}", id);
+        return ResponseEntity.ok(blog);
     }
 
     @GetMapping
     public ResponseEntity<List<TechBlog>> getAllBlogs() {
-        return ResponseEntity.ok(techBlogService.getAllBlogs());
+        logger.debug("Received request to fetch all blog posts");
+        List<TechBlog> blogs = techBlogService.getAllBlogs();
+        logger.info("Successfully retrieved {} blog posts", blogs.size());
+        return ResponseEntity.ok(blogs);
     }
 
     @GetMapping("/topic/{topic}")
     public ResponseEntity<List<TechBlog>> getBlogsByTopic(@PathVariable String topic) {
-        return ResponseEntity.ok(techBlogService.getBlogsByTopic(topic));
+        logger.debug("Received request to fetch blog posts for topic: {}", topic);
+        List<TechBlog> blogs = techBlogService.getBlogsByTopic(topic);
+        logger.info("Successfully retrieved {} blog posts for topic: {}", blogs.size(), topic);
+        return ResponseEntity.ok(blogs);
     }
 
     @GetMapping("/author/{author}")
     public ResponseEntity<List<TechBlog>> getBlogsByAuthor(@PathVariable String author) {
-        return ResponseEntity.ok(techBlogService.getBlogsByAuthor(author));
+        logger.debug("Received request to fetch blog posts by author: {}", author);
+        List<TechBlog> blogs = techBlogService.getBlogsByAuthor(author);
+        logger.info("Successfully retrieved {} blog posts by author: {}", blogs.size(), author);
+        return ResponseEntity.ok(blogs);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBlog(@PathVariable Long id) {
+        logger.debug("Received request to delete blog post with id: {}", id);
         techBlogService.deleteBlog(id);
+        logger.info("Successfully deleted blog post with id: {}", id);
         return ResponseEntity.noContent().build();
     }
 } 
--- src/main/java/com/techblogs/service/impl/TechBlogServiceImpl.java
+++ src/main/java/com/techblogs/service/impl/TechBlogServiceImpl.java
@@ -5,6 +5,8 @@ import com.techblogs.repository.TechBlogRepository;
 import com.techblogs.service.TechBlogService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,56 +17,82 @@ import java.util.List;
 @Transactional
 public class TechBlogServiceImpl implements TechBlogService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TechBlogServiceImpl.class);
     private final TechBlogRepository techBlogRepository;
 
     @Override
     public TechBlog createBlog(TechBlog techBlog) {
-        return techBlogRepository.save(techBlog);
+        logger.debug("Creating new blog post with title: {}", techBlog.getTitle());
+        TechBlog savedBlog = techBlogRepository.save(techBlog);
+        logger.info("Successfully created blog post with id: {}", savedBlog.getId());
+        return savedBlog;
     }
 
     @Override
     public TechBlog updateBlog(Long id, TechBlog techBlog) {
+        logger.debug("Updating blog post with id: {}", id);
         TechBlog existingBlog = techBlogRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Blog not found with id: " + id));
+                .orElseThrow(() -> {
+                    logger.error("Blog post not found with id: {}", id);
+                    return new EntityNotFoundException("Blog not found with id: " + id);
+                });
         
         existingBlog.setTitle(techBlog.getTitle());
         existingBlog.setContent(techBlog.getContent());
         existingBlog.setAuthor(techBlog.getAuthor());
         existingBlog.setTopic(techBlog.getTopic());
         
-        return techBlogRepository.save(existingBlog);
+        TechBlog updatedBlog = techBlogRepository.save(existingBlog);
+        logger.info("Successfully updated blog post with id: {}", id);
+        return updatedBlog;
     }
 
     @Override
     @Transactional(readOnly = true)
     public TechBlog getBlogById(Long id) {
+        logger.debug("Fetching blog post with id: {}", id);
         return techBlogRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Blog not found with id: " + id));
+                .orElseThrow(() -> {
+                    logger.error("Blog post not found with id: {}", id);
+                    return new EntityNotFoundException("Blog not found with id: " + id);
+                });
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<TechBlog> getAllBlogs() {
-        return techBlogRepository.findAll();
+        logger.debug("Fetching all blog posts");
+        List<TechBlog> blogs = techBlogRepository.findAll();
+        logger.info("Retrieved {} blog posts", blogs.size());
+        return blogs;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<TechBlog> getBlogsByTopic(String topic) {
-        return techBlogRepository.findByTopic(topic);
+        logger.debug("Fetching blog posts for topic: {}", topic);
+        List<TechBlog> blogs = techBlogRepository.findByTopic(topic);
+        logger.info("Retrieved {} blog posts for topic: {}", blogs.size(), topic);
+        return blogs;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<TechBlog> getBlogsByAuthor(String author) {
-        return techBlogRepository.findByAuthor(author);
+        logger.debug("Fetching blog posts by author: {}", author);
+        List<TechBlog> blogs = techBlogRepository.findByAuthor(author);
+        logger.info("Retrieved {} blog posts by author: {}", blogs.size(), author);
+        return blogs;
     }
 
     @Override
     public void deleteBlog(Long id) {
+        logger.debug("Attempting to delete blog post with id: {}", id);
         if (!techBlogRepository.existsById(id)) {
+            logger.error("Blog post not found with id: {}", id);
             throw new EntityNotFoundException("Blog not found with id: " + id);
         }
         techBlogRepository.deleteById(id);
+        logger.info("Successfully deleted blog post with id: {}", id);
     }
 } 
